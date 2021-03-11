f8712e0bd565988b1377e23b588ad546ba492619
shaejohnston <johnstke@oregonstate.edu>
2021-03-11 14:34:13 -0600

    CSS fixes to look more presentable

--- SQL_Related/data_manipulation.sql
+++ SQL_Related/data_manipulation.sql
@@ -7,7 +7,7 @@ SELECT * FROM Authors;
 SELECT * FROM Genres;
 
 
--- Thanks for figuring out how to do this Scott!
+-- Home Page Select w/ Joins
 SELECT Books.bookID, Books.title, GROUP_CONCAT(DISTINCT Genres.genreName ORDER BY Genres.genreName) as 'Genre', Authors.authorName, Books.publicationDate, Books.patronID
 FROM Books
 JOIN Books_Genres ON Books.bookID = Books_Genres.bookID
